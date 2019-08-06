8cc967151deeb615405aeb8b39f76614f7aec5e2
egvo <gvozdikov@like-coffee.ru>
2019-08-06 21:13:45 +0400

    Removed all impoorts to __init__.

--- __init__.py
+++ __init__.py
@@ -4,11 +4,3 @@
 """
 Модуль с универсальными классами и функциями
 """
-
-from universal.universal import join_nonempty
-from universal.date_and_time import duration, duration_format, localize, utcnow, \
-    as_timezone, day_start, next_day, week_start, next_week, month_start, next_month, tz_offset_in_ms
-from universal.list_of_dicts import find_dict_in_list, sort_list_of_dicts
-from universal.yandex_mailer import YandexMailer
-from universal.DottedDict import DottedDict
-from universal.MongoAggregation import MongoAggregation
