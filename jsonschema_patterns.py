29c954d83728362b6ac8bb680241e4c459bb5dc9
egvo <gvozdikov@like-coffee.ru>
2019-10-23 16:00:28 +0400

    Изменил формат возвращаемых данных jsonschema_patterns.validate.

--- jsonschema_patterns.py
+++ jsonschema_patterns.py
@@ -11,21 +11,16 @@ from jsonschema import Draft4Validator, FormatChecker
 def validate(data, scheme):
     validator = Draft4Validator(scheme, format_checker=FormatChecker())
     errors = sorted(validator.iter_errors(data), key=lambda e: e.path)
-    path_errors = {}
-    simple_errors = []
+    result_errors = {}
     for error in iter(errors):
         error_path = '.'.join(str(path) for path in error.absolute_path)
         message = error.message.replace(r"u'", "'")
         if error_path:
-            path_errors[error_path] = message
+            result_errors[error_path] = message
         else:
-            simple_errors.append(message)
-    if simple_errors and path_errors:
-        simple_errors.append(path_errors)
-        return simple_errors
-    elif simple_errors:
-        return simple_errors
-    return path_errors
+            result_errors.setdefault('_', [])
+            result_errors['_'].append(message)
+    return result_errors
 
 
 nonempty_string = {
@@ -62,7 +57,7 @@ date_ISO8601_Z_pattern = {
     'type': 'string',
     'minLength': 20,
     'maxLength': 20,
-    'pattern': '^20[0-9]{2}-[0-1][0-9]-[0-3][0-9]T[0-2][0-9]:[0-6][0-9]:[0-6][0-9]Z$',
+    'pattern': '^20[0-9]{2}-[0-1][0-9]-[0-3][0-9]T[0-2][0-9]:[0-6][0-9]:[0-6][0-9]Z?$',
 }
 
 int_as_string = {
